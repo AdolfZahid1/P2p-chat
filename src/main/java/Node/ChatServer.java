c473f9507de96402070b20db6bc300e3fe731db8
Kirils Obodņikovs <k.riobod@gmail.com>
2024-05-25 18:55:49 +0300

    Initial commit

---
+++ src/main/java/Node/ChatServer.java
@@ -0,0 +1,72 @@
+package Node;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+    private int port;
+    private Set<String> userNames = new HashSet<>();
+    private Set<UserThread> userThreads = new HashSet<>();
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    public void execute() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Chat Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected");
+
+                UserThread newUser = new UserThread(socket, this);
+                userThreads.add(newUser);
+                newUser.start();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error in the server: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("No port was provided, using default port 8818");
+            new ChatServer(8818).execute();
+        } else {
+            new ChatServer(Integer.parseInt(args[0])).execute();
+        }
+    }
+
+    void broadcast(String message, UserThread excludeUser) {
+        for (UserThread aUser : userThreads) {
+            if (aUser != excludeUser) {
+                aUser.sendMessage(message);
+            }
+        }
+    }
+
+    void addUserName(String userName) {
+        userNames.add(userName);
+    }
+
+    void removeUser(String userName, UserThread aUser) {
+        boolean removed = userNames.remove(userName);
+        if (removed) {
+            userThreads.remove(aUser);
+            System.out.println("The user " + userName + " quitted");
+        }
+    }
+
+    Set<String> getUserNames() {
+        return this.userNames;
+    }
+
+    boolean hasUsers() {
+        return !this.userNames.isEmpty();
+    }
+
+}
